e65b3e0ae97d18ae69bc2aa833976865cc1094b4
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2015-12-18 17:17:42 +0000

    More tests inside the container.

--- kerberos-lxc/configureContainer.sh
+++ kerberos-lxc/configureContainer.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+lxc-create -t download -n kerberos -- --dist debian --release jessie --arch amd64
+
 cat /var/lib/lxc/kerberos/config
 
 chroot /var/lib/lxc/kerberos/rootfs /bin/bash --login
 
+echo "Inside chroot"
+
+cat /etc/hostname
+
 ip a
+
+ping 8.8.8.8
+
+echo "Going to install Kerberos packages"
 
 apt-get update
 apt-get install -y krb5-user krb5-kdc krb5-admin-server krb5-kdc-ldap
@@ -12,3 +22,5 @@ apt-get clean
 
 service krb5-kdc enable
 service krb5-admin-server enable
+
+lxc-start -n kerberos

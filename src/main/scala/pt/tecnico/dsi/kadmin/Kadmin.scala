b8b71aeb290661e9bc756d87a814deb4d9cafe8d
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2016-01-20 14:28:13 +0000

    UnknownError back in everything.

--- src/main/scala/pt/tecnico/dsi/kadmin/Kadmin.scala
+++ src/main/scala/pt/tecnico/dsi/kadmin/Kadmin.scala
@@ -66,7 +66,7 @@ class Kadmin(val settings: Settings = new Settings()) extends LazyLogging {
     e.expect
       .addWhen(passwordIncorrect)
       .addWhen(passwordExpired)
-      //.addWhen(unknownError)
+      .addWhen(unknownError)
       .when(EndOfFile)
         .returningExpect {
           val datetimeRegex = """\d\d-\d\d-\d\d\d\d \d\d:\d\d:\d\d"""
@@ -126,8 +126,8 @@ class Kadmin(val settings: Settings = new Settings()) extends LazyLogging {
         //All good. The password was correct. We can continue.
         //We need to send a newline in order for `f` to see the KadminPrompt
         .sendln("")
-      /*.addWhen(unknownError)
-        .addActions(preemptiveExit)*/
+      .addWhen(unknownError)
+        .addActions(preemptiveExit)
     e.addExpectBlock(f)
     e.expect(kadminPrompt)
       .sendln("quit")
@@ -162,8 +162,8 @@ class Kadmin(val settings: Settings = new Settings()) extends LazyLogging {
         //All good. We can continue.
         //We need to send a newline in order for `f` to see KadminPrompt
         .sendln("")
-      /*.addWhen(unknownError)
-        .addActions(preemptiveExit)*/
+      .addWhen(unknownError)
+        .addActions(preemptiveExit)
     e.addExpectBlock(f)
     e.expect(kadminPrompt)
       .sendln("quit")
@@ -231,7 +231,7 @@ class Kadmin(val settings: Settings = new Settings()) extends LazyLogging {
           //
           //By modifying we could run into troubles if -randkey or -pw is used.
         .addWhen(insufficientPermission("add"))
-        //.addWhen(unknownError)
+        .addWhen(unknownError)
     }
   }
 
@@ -268,7 +268,7 @@ class Kadmin(val settings: Settings = new Settings()) extends LazyLogging {
         .returning(Right(true))
         .addWhen(insufficientPermission("inquire"))
         .addWhen(insufficientPermission("changepw"))
-        //.addWhen(unknownError)
+        .addWhen(unknownError)
     }
   }
 
@@ -374,8 +374,8 @@ class Kadmin(val settings: Settings = new Settings()) extends LazyLogging {
             .addActions(preemptiveExit)
           .addWhen(insufficientPermission("modify"))
             .addActions(preemptiveExit)
-          /*.addWhen(unknownError)
-            .addActions(preemptiveExit)*/
+          .addWhen(unknownError)
+            .addActions(preemptiveExit)
       }
       e.expect(kadminPrompt)
         .sendln( s"""modify_principal $cleanedOptions $fullPrincipal""")
@@ -387,7 +387,7 @@ class Kadmin(val settings: Settings = new Settings()) extends LazyLogging {
         //was cleared these cases will already have been caught.
         w.addWhen(principalDoesNotExist)
           .addWhen(insufficientPermission("modify"))
-          //.addWhen(unknownError)
+          .addWhen(unknownError)
       }
     }
   }
@@ -648,7 +648,7 @@ class Kadmin(val settings: Settings = new Settings()) extends LazyLogging {
           .returning(Left(PasswordIsBeingReused))
         .addWhen(principalDoesNotExist)
         .addWhen(insufficientPermission("changepw"))
-        //.addWhen(unknownError)
+        .addWhen(unknownError)
     }
   }
 

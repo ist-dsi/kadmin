50fc4ef3a46cfa30582efb7699b3362fb6299ca2
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2016-03-31 19:16:05 +0100

    Update fake submodule.
    Create test.sh and change .travis.yml to be able to test the code in
    travis.

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sbt compile
+
+cd docker-kerberos
+
+# Build the containers
+docker-compose build
+
+# Run them. The --abort-on-container-exit stops all containers if any container was stopped.
+docker-compose up --abort-on-container-exit
+EXIT=$(docker-compose ps | grep "kerberos-client" | grep -oP "(?<=Exit )\d+")
+
+# Remove the containers to ensure a clean slate the next time this script in ran.
+docker-compose rm -f
+
+exit $EXIT

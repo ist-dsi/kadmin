a8851a0190706f382b232fe6360f753469cadaeb
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2016-04-01 19:04:50 +0100

    Changed exit code computation.

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ docker-compose build
 
 # Run them. The --abort-on-container-exit stops all containers if any container was stopped.
 docker-compose up --abort-on-container-exit
-EXIT=$(docker-compose ps | grep "kerberos-client" | grep -oP "(?<=Exit )\d+")
+EXIT_CODE=$(docker-compose ps "kerberos-client" | grep -oP "(?<=Exit )\d+")
 
 # Remove the containers to ensure a clean slate the next time this script in ran.
 docker-compose rm -f
 
-exit $EXIT
+exit $EXIT_CODE
